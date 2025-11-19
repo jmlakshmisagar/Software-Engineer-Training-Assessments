e161f1d50e51f972b815c41f97264fda1e2a95af
Lakshmisagar J M <152427404+jmlakshmisagar@users.noreply.github.com>
2025-11-19 19:25:24 +0530

    Used order by clause

---
+++ 19-Nov-2025/order_by.sql
@@ -0,0 +1,19 @@
+25. select * from customers
+    where country = 'france'
+    order by creditlimit desc;
+
+26. select * from products
+    order by quantityinstock asc;
+
+27. select * from payments
+    order by paymentdate desc;
+
+28. select * from employees
+    order by jobtitle, lastname;
+
+29. select * from customers
+    order by creditlimit desc
+    limit 5;
+
+30. select * from productlines
+    order by productline;
