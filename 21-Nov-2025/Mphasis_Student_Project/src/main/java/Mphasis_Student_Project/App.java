9ffa478c4759d7324c32f7d73990de6816fc2062
jmlakshmisagar <jmlakshmisagar@gmail.com>
2025-11-21 15:02:17 +0530

    Commiting Student Project

---
+++ 21-Nov-2025/Mphasis_Student_Project/src/main/java/Mphasis_Student_Project/App.java
@@ -0,0 +1,10 @@
+package Mphasis_Student_Project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
