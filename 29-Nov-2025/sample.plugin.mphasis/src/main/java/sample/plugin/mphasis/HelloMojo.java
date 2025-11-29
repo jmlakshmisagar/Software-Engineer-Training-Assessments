338e10b3631c0900a8b7d94098d9aa764f8d9b33
jmlakshmisagar <jmlakshmisagar@gmail.com>
2025-11-29 17:33:18 +0530

    Work on Custom plugins using maven build and mojo

---
+++ 29-Nov-2025/sample.plugin.mphasis/src/main/java/sample/plugin/mphasis/HelloMojo.java
@@ -0,0 +1,13 @@
+
+package sample.plugin.mphasis;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "hello")
+public class HelloMojo extends AbstractMojo {
+	public void execute() throws MojoExecutionException {
+		getLog().info("Hello from custom Maven plugin!");
+	}
+}
